92eaa991829d97c4816e58c1675131d2a50b761b
Gabe Fierro <gtfierro225@gmail.com>
2011-10-24 15:59:54 -0700

    almost done with adaptivesubdividepatch. We should put it into the scene.cpp file so we know how to access all the methods

--- as5/bez.cpp
+++ as5/bez.cpp
@@ -40,7 +40,6 @@ TriMesh adaptivesubdividepatch(QuadMesh patch, float error) {
 				quad.empty();
 				uvs.empty();
 			}
-			//TODO: when we push_back a quad, push_back the u,v values as well
 			quad.push_back(patch.getVert(i+4*j));
 			uvs.push_back(vec2(i/4.0f,j/4.0f)); // u,v as canonical values
 		}
@@ -59,10 +58,11 @@ TriMesh adaptivesubdividepatch(QuadMesh patch, float error) {
 
 //given a patch, perform adaptive subdivision with triangles
 TriMesh adaptivesubdividepatch(TriMesh patch, float error) {
-	vec3 curve[3];
+//	vec3 curve[3]; // use patch.vertsVec
+//	vec2 uvs[3]; // use patch.uvValues
 	vec3 splitEdges[3];
 	vec3 trianglePoint[3];
-	vec3 bezierPoint;
+	vec3 bezierPoint[3];
 	// need to do the following for all 3 sides of the triangle
 	// if the bezierPoint is some error away from the triangle midpoint
 	//	then we create a new vertex, and split the triangle around that one
@@ -71,19 +71,19 @@ TriMesh adaptivesubdividepatch(TriMesh patch, float error) {
 	//	p1,p2 is edge 1
 	// 	p2,p0 is edge 2
 	
-	//TODO: get curve from TriMesh patch
-	//TODO: want to iterate through some u,v for TriMesh
-	//TODO: generate bezierPoint for each side of the Triangle
+	trianglePoint[0] = 0.5f * (patch.vertsVec[0]+patch.vertsVec[1]);
+	trianglePoint[1] = 0.5f * (patch.vertsVec[1]+patch.vertsVec[2]);
+	trianglePoint[2] = 0.5f * (patch.vertsVec[2]+patch.vertsVec[0]);
 	
-	trianglePoint[0] = 0.5f * (curve[0]+curve[1]);
-	trianglePoint[1] = 0.5f * (curve[1]+curve[2]);
-	trianglePoint[2] = 0.5f * (curve[2]+curve[0]);
+	bezierPoint[0] = bezpatchinterp(patch,patch.uvValues[0][0],patch.uvValues[0][1]);
+	bezierPoint[1] = bezpatchinterp(patch,patch.uvValues[1][0],patch.uvValues[1][1]);
+	bezierPoint[2] = bezpatchinterp(patch,patch.uvValues[2][0],patch.uvValues[2][1]);
 	
 	for (int i = 0; i < 3; i++) {
-		if (error > (bezierPoint - trianglePoint[i]).length2()) {
+		if (error > (bezierPoint[i] - trianglePoint[i]).length2()) {
 			splitEdges[i] = NULL;
 		} else {
-			splitEdges[i] = bezierPoint;
+			splitEdges[i] = bezierPoint[i];
 		}
 	}
 	// more stuff coming soon
