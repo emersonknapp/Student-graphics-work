ec7066c90ccce7e499f38e718a6a9b3e09f2582c
Emerson Knapp <emerson.b.knapp@gmail.com>
2011-10-24 17:01:58 -0700

    try running it on teapot

--- as5/as5.cpp
+++ as5/as5.cpp
@@ -63,36 +63,14 @@ void myDisplay() {
 	
 	glLoadIdentity();
 	
-	glTranslatef(0,0,0);
+	glTranslatef(0,0,-10);
 	for (int i=0; i<scene->quadmeshes.size(); i++) {
 		QuadMesh* mesh = scene->quadmeshes[i];
-		
-		glBegin(GL_POINTS);
-		glColor3f(1,1,1);
-		for (int j=0; j<mesh->n_poly; j++) {
-			vec4 q;
-			vec3 n;
-			for (int k=0; k<4; k++) {
-				
-				int q = mesh->indices[j*4+k];
-				//vec3 n = vec3(mesh->norms[q*4], mesh->norms[q*4+1], mesh->norms[q*4+2]);
-				//vec3 v = vec3(mesh->verts[q*4], mesh->verts[q*4+1], mesh->verts[q*4+2]);
-				vec3 n = mesh->normsVec[q];
-				vec3 v = mesh->vertsVec[q];
-				//cout << n << v << endl;
-				glNormal3f(n[0], n[1], n[2]);
-				glVertex3f(v[0], v[1], v[2]);	
-			}
-			//cout << endl << endl;
-		}
-		//cout << "**********************************" << endl;
-		glEnd();
-		
-		//glColor3f(1,1,1);
-		//glVertexPointer(3, GL_FLOAT, 0, mesh->verts);
-		//glNormalPointer(GL_FLOAT, 0, mesh->norms);
-		//glDrawElements(GL_QUADS, 4*mesh->n_poly, GL_UNSIGNED_INT, mesh->indices);
-		
+
+		glColor3f(.2,.2,.2);
+		glVertexPointer(3, GL_FLOAT, 0, mesh->verts);
+		glNormalPointer(GL_FLOAT, 0, mesh->norms);
+		glDrawElements(GL_QUADS, 4*mesh->n_poly, GL_UNSIGNED_INT, mesh->indices);
 	}
 	
 	glFlush();
@@ -160,6 +138,9 @@ void initScene(){
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClearDepth(1.0f);
 	glEnable(GL_DEPTH_TEST);
+	glEnableClientState(GL_INDEX_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glEnableClientState(GL_NORMAL_ARRAY);
 	glDepthFunc(GL_LEQUAL);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	GLfloat LightAmbient[]= {1, 1, 1, 1};
